7aae6093a0e56a032e13a4522958eae5b5c10121
katyamarakhovskaya <katyamarakhovskaya@gmail.com>
2019-06-01 22:03:07 +0300

    adaptive 3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <div className="Header">
         <div className="conteiner_head">
       <h1><span> CYBERIA </span></h1>
-         <a type="button" href="#" class="header_button">Присоедитится</a>*
+         <a type="button" href="https://t.me/joinchat/F2M0_AtESEE1hZWs7FJSUQ" class="header_button">Присоедитится</a>*
         {/*<img src={process.env.PUBLIC_URL + '/hackerheader1.jpg'  } alt="header" className="headerimg"/>*/}
         </div>
       </div>
